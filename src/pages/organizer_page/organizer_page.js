8781be3205f35bb6e26c7882d36ca11a24b506db
reuben21 <reuben211999@icloud.com>
2022-01-23 02:57:51 +0530

    Updated Code with Chat functions

---
+++ src/pages/organizer_page/organizer_page.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {makeStyles, withStyles} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import {TextField} from "@material-ui/core";
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+
+const ColorButton = withStyles((theme) => ({
+    root: {
+        color: '#325288 !important',
+        fontFamily: "'Montserrat', sans-serif",
+        fontSize: "15px",
+        margin:5,
+        width:"100%",
+        textTransform:"none",
+        border:"1px solid #325288"
+    },
+}))(Button);
+
+
+
+
+const useStyles = makeStyles({
+    root: {
+        overflow: 'auto',
+        height:"82vh",
+        backgroundColor:"white",
+        margin:10
+    },
+    paper: {
+        position: 'absolute',
+        width: 300,
+        display:"flex",
+        flexDirection:"column",
+
+        backgroundColor: "#FAEEE7",
+        border: '2px solid #000',
+        boxShadow: "#FAEEE7",
+        padding: 10,
+    },
+});
+
+export default function OrganizerPage(props) {
+    const [roomName, setRoomName] = React.useState("");
+    const classes = useStyles();
+
+
+
+    return (
+        <Card className={classes.root}>
+
+
+            {props.sub_channels.map((value)=>{
+                return <ColorButton onClick={()=>props.changeRooms(value)} style={{
+                    border: '0px',
+
+                }} size="small">{value.sub_channel_name}</ColorButton>
+            })}
+
+        </Card>
+    );
+}
