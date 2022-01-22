85ea90420edb8ad426af4d9f0c28681ed957db2e
Harsh Gandhi <harsh@Harshs-MacBook-Air.local>
2022-01-22 22:55:20 +0530

    overview

--- src/component/hackheader/hackheader.js
+++ src/component/hackheader/hackheader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BiTime } from 'react-icons/bi';
 
 export default function Hackheader() {
 
@@ -9,7 +10,22 @@ export default function Hackheader() {
                 height: '40vh',
         }}/>
     
-    <h2></h2>
+    <div 
+       style={{
+        position: 'absolute',
+        left: '30px',
+        top: '180px',
+      }}
+    >
+    <div className="bg-success align-items-center text-light p-1">
+      <BiTime size={30}/> Ends on 31th Jan'22 09:00 PM (India Standard Time)
+    </div>
+    <h3 className="text-light mt-1"
+     
+    >Data Sprint 62 - Weather Recognition</h3>
+    </div>
+
+    
 
   </div>
   );
