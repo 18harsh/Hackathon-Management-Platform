64b79956ce637a6fb31a0a3d968d549c6625d29d
Harsh Gandhi <harsh@Harshs-MacBook-Air.local>
2022-01-22 20:39:11 +0530

    minor chanes

--- src/component/tabs/tabs.js
+++ src/component/tabs/tabs.js
@@ -6,11 +6,25 @@ import Button from "@material-ui/core/Button";
 export default function Tabs(props) {
     let { hackathonId } = useParams();
 
-    return (<div>
+    return (<div className="d-flex justify-content-around">
         <div>
                 <Button color="inherit" component={NavLink} to={`/hackathon/overview/${hackathonId}`}>Overview</Button>
                 <Button color="inherit" component={NavLink} to={`/hackathon/discussion/${hackathonId}`}>Discussion</Button>
+                <Button color="inherit" component={NavLink} to={`/hackathon/discussion/${hackathonId}`}>Notebook</Button>
 
+        </div>
+        <div>
+        <ColorButton size="small" component={NavLink} to={ props?.user?.email ? `/hackathon/${props.hackathonId}` : `/authenticate`} className="bg-success w-100 ml-auto" style={{
+                            borderRadius: '50px',
+                        }}
+                        >
+                        <span className="text-light" style={{
+                        fontSize: '12px',
+                        fontWeight: 'bold'
+                    }}>
+                        Participate Now
+                        </span>
+        </ColorButton>
         </div>
         </div>
         );
